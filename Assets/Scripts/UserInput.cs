5a6d886a0788bdbd28031d259b751fbfa69397bb
Aaron Villalobos <avilla4537@gmail.com>
2021-01-17 22:51:52 -0800

    Added values variable
    Variable of string array that will hold the cards.

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+    public static string[] values = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25" };
+
     // Start is called before the first frame update
     void Start()
     {
-        public GameObject deck;
+        
     }
 
     // Update is called once per frame
